beca0a38ef0f7882eaba36c0e7eb8f691a7dee54
Hien Luong <hyhien@gmail.com>
2020-05-18 08:41:03 +0700

    18May - Done Insert flower

--- 18may/BT02_them_bo_hoa.php
+++ 18may/BT02_them_bo_hoa.php
@@ -32,14 +32,22 @@ if(isset($_FILES["Hinh"]) && $_FILES["Hinh"]["error"] == 0)
 	if(move_uploaded_file($_FILES["Hinh"]["tmp_name"], "hoa/".$_FILES["Hinh"]["name"]))
 	{
 		$tenhoa = $_REQUEST["TenBoHoa"];
+		$gia = number_format($_REQUEST["GiaBoHoa"]);
 		$chuoi = <<< EOD
 		<h2>Đã thêm thành công</h2>
 		<div style="text-align:center;">
 			<img src="hoa/{$_FILES["Hinh"]["name"]}" alt="{$tenhoa}" /><br>
 			<h3>{$tenhoa}</h3>
+			<h4>{$gia} đ</h4>
 		</div>
 EOD;
 		echo $chuoi;
+		
+		//Ghi xuống file
+		$chuoi_ghi_xuong = "/*{$tenhoa}|{$_REQUEST["GiaBoHoa"]}|{$_FILES["Hinh"]["name"]}";
+		$file = fopen("hoa_xuan.txt", "at");
+		fwrite($file, $chuoi_ghi_xuong);
+		fclose($file);
 	}
 }
 ?>
