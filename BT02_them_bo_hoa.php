e22b35cffc39c332f57c3127b07ed7e5931afae4
Hien Luong <hyhien@gmail.com>
2020-05-21 07:55:39 +0700

    Init

---
+++ BT02_them_bo_hoa.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+	<link href="css/bootstrap/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+	<h1>THEM BO HOA</h1>
+	<table>
+		<tr>
+			<td>Ten bo hoa</td>
+			<td>
+				<input name="TenBoHoa" />
+			</td>
+		</tr>
+		<tr>
+			<td>Gia bo hoa</td>
+			<td>
+				<input name="GiaBoHoa" type="number"/>
+			</td>
+		</tr>
+		<tr>
+			<td>Hinh</td>
+			<td>
+				<input name="Hinh" type="file" />
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<button>Them bo hoa</button>
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 1921COMP1303
 Source code khóa "Phát triển Ứng dụng Web" HK2 - NH 2019.2020
 
-### Source code từng buổi vui lòng chọn nhánh tương ứng rồi chọn "Clone or Download" và "Download ZIP". Ngoài ra, SV có thể dùng công cụ để download từng branch.
+### 21-May-2020 Upload file, Đọc/ghi file
