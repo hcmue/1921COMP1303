a364594c3705daaa03bad4e95dba779a23d57d83
Hien Luong <hyhien@gmail.com>
2020-05-23 09:05:13 +0700

    done BT02 - Lab06 - them hoa

--- Lab06.BT02.ThemHoa.php
+++ Lab06.BT02.ThemHoa.php
@@ -1,17 +1,17 @@
-<form medthod="post" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data">
 	<h2>THÊM BÓ HOA</h2>
 	<table>
 		<tr>
 			<td>Tên bó hoa</td>
-			<td><input name="TenBoHoa"></td>
+			<td><input name="TenBoHoa" required></td>
 		</tr>
 		<tr>
 			<td>Giá bó hoa</td>
-			<td><input name="GiaBoHoa"></td>
+			<td><input name="GiaBoHoa" required></td>
 		</tr>
 		<tr>
 			<td>Hình</td>
-			<td><input name="Hinh" type="file"></td>
+			<td><input name="Hinh" type="file" required></td>
 		</tr>
 		<tr>
 			<td colspan="2">
@@ -20,3 +20,33 @@
 		</tr>
 	</table>
 </form>
+<?php
+if(isset($_FILES["Hinh"]) && $_FILES["Hinh"]["error"] == 0)
+{
+	if(move_uploaded_file($_FILES["Hinh"]["tmp_name"], "hoa/".$_FILES["Hinh"]["name"]))
+	{
+		$gia = number_format($_REQUEST["GiaBoHoa"]);
+		$chuoi = <<< EOD
+		<div>
+			<img src="hoa/{$_FILES["Hinh"]["name"]}" alt="{$_REQUEST["TenBoHoa"]}" />
+			<h3>{$_REQUEST["TenBoHoa"]}</h3>
+			<h3>{$gia} đ</h3>
+		</div>
+EOD;
+		echo $chuoi;
+
+		//Lưu file
+		$file = fopen("hoa_xuan.txt", "a");
+		fwrite($file, "/*{$_REQUEST['TenBoHoa']}|{$_REQUEST['GiaBoHoa']}|{$_FILES['Hinh']['name']}");
+		fclose($file);
+	}
+}
+
+?>
+
+
+
+
+
+
+
