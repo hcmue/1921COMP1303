e10f19d48b6e6d28358e6d88fe5b9123482b1507
Hien Luong <hyhien@gmail.com>
2020-06-01 09:09:09 +0700

    Done AJAX

--- ajax/Hoa.php
+++ ajax/Hoa.php
@@ -5,18 +5,40 @@
     <meta charset="utf-8" />
     <title></title>
     <script src="../js/jquery/jquery-3.5.0.js"></script>
+    <script>
+    function LayHoa(){
+        $.ajax({
+            url: "LayHoa.php",
+            data: { MaLoai: $("#LoaiHoa").val()},
+            success: function(data){
+                console.log(data);
+                $("#dsHoa").html(data);
+            }
+        });
+    }    
+    $(function(){
+        LayHoa();
+        $("#LoaiHoa").change(function(){
+            LayHoa();
+        });   
+    });
+    </script>
 </head>
 <body>
     <?php
-    include_once("DataProvider.php");
+    include_once("../DataProvider.php");
 $ds_loai_hoa = DataProvider::ExecuteQuery("SELECT MaLoai, TenLoai FROM loaihoa");
 ?>
-<select name="LoaiHoa" class="form-control">
+<select id="LoaiHoa" class="form-control">
     <?php
     while($loai = mysqli_fetch_array($ds_loai_hoa)){
         echo "<option value='{$loai['MaLoai']}'>{$loai['TenLoai']}</option>";
     }
     ?>
+</select>
 <div id="dsHoa"></div>
+<style>
+.hoa{width:80px; height: 100px;}
+</style>
 </body>
 </html>
---
+++ ajax/LayHoa.php
@@ -0,0 +1,36 @@
+﻿<?php
+$loai_hoa = @$_REQUEST["MaLoai"];
+include_once("../DataProvider.php");
+$query = "SELECT MaHoa, TenHoa, GiaBan, Hinh FROM hoa";
+if(isset($loai_hoa))
+{
+	$query .= " WHERE MaLoai = $loai_hoa";
+}
+$query .= " ORDER BY GiaBan";
+?>
+<table border="1" cellspacing="0" cellpadding="5">
+	<tr>
+		<th>STT</th>
+		<th>Hình</th>
+		<th>Hoa</th>
+		<th>Giá bán</th>
+	<tr>
+	<?php
+	$result = DataProvider::ExecuteQuery($query);
+	$stt = 1;
+	while($row = mysqli_fetch_array($result))
+	{
+		$gia = number_format($row['GiaBan']);
+		$chuoi = <<< EOD
+	<tr>
+		<td>{$stt}</td>
+		<td><img src="../hoa/{$row['Hinh']}" class="hoa" /></td>
+		<td>{$row['TenHoa']}</td>
+		<td>{$gia} đ</td>
+	<tr>
+EOD;
+		echo $chuoi; $stt++;
+	}
+	?>
+
+</table>
