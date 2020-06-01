b839744ba30c7510ba10db97e8250240fc58f63b
Hien Luong <hyhien@gmail.com>
2020-06-01 08:49:53 +0700

    Ajax

---
+++ ajax/Hoa.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <script src="../js/jquery/jquery-3.5.0.js"></script>
+</head>
+<body>
+    <?php
+    include_once("DataProvider.php");
+$ds_loai_hoa = DataProvider::ExecuteQuery("SELECT MaLoai, TenLoai FROM loaihoa");
+?>
+<select name="LoaiHoa" class="form-control">
+    <?php
+    while($loai = mysqli_fetch_array($ds_loai_hoa)){
+        echo "<option value='{$loai['MaLoai']}'>{$loai['TenLoai']}</option>";
+    }
+    ?>
+<div id="dsHoa"></div>
+</body>
+</html>
