0638d58ff95b336f95ba5c81798e55231163d591
Hien Luong <hyhien@gmail.com>
2020-06-06 09:12:02 +0700

    Done - Ajax get flowers

---
+++ ajax/XLLayHoa.php
@@ -0,0 +1,35 @@
+﻿<table>
+	<tr>
+		<th>STT</th>
+		<th>Hình</th>
+		<th>Tên hoa</th>
+		<th>Giá bán</th>
+	</tr>
+<?php
+	include_once("../DataProvider.php");
+	$sql = "SELECT MaHoa, TenHoa, GiaBan, Hinh FROM hoa";
+	if(isset($_REQUEST["ma_loai_hoa"]))
+	{
+		$sql = $sql." WHERE MaLoai = ".$_REQUEST["ma_loai_hoa"];
+	}
+	//echo $sql;
+	$result = DataProvider::ExecuteQuery($sql);
+	$i = 1;
+	while($row = mysqli_fetch_array($result))
+	{
+		$gia = number_format($row["GiaBan"]);
+		$chuoi = <<< EOD
+	<tr>
+		<td>{$i}</td>
+		<td>
+			<img src="../hoa/{$row["Hinh"]}" height="100" />
+		</td>
+		<td>{$row["TenHoa"]}</td>
+		<td>{$gia} đ</td>
+	</tr>
+EOD;
+		echo $chuoi; $i++;
+	}
+?>
+	
+</table>
---
+++ ajax/XemDSHoa.php
@@ -0,0 +1,40 @@
+﻿<?php
+include_once("../DataProvider.php");
+$sql_loai = "SELECT MaLoai, TenLoai FROM loaihoa";
+$dsHoa = DataProvider::ExecuteQuery($sql_loai);
+?>
+<h2>DANH SÁCH</h2>
+Loại hoa:
+<select id="LoaiHoa">
+<?php
+while($loai = mysqli_fetch_array($dsHoa))
+{
+	echo '<option value="'.$loai["MaLoai"].'">'.$loai["TenLoai"].'</option>';
+}
+?>
+</select>
+<div id="danh_sach_hoa"></div>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script>
+function layHoaTheoLoai(){
+	$.ajax({
+		url: "XLLayHoa.php",
+		type: "POST",
+		data: { 
+			"ma_loai_hoa": $("#LoaiHoa").val()
+		},
+		success: function(data){
+			$("#danh_sach_hoa").html(data);
+		},
+		error: function(){
+			alert("Lỗi xử lý");
+		}
+	});
+}
+$(function(){
+	layHoaTheoLoai();
+	$("#LoaiHoa").change(function(){
+		layHoaTheoLoai();
+	});
+});
+</script>
