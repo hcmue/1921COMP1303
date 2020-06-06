5ae5432b94c7d52426255724b252d19410e07061
Hien Luong <hyhien@gmail.com>
2020-06-06 10:20:45 +0700

    Complete get flower by PDO

--- PDO/Lab08.php
+++ PDO/Lab08.php
@@ -1,10 +1,31 @@
-<?php
+﻿<?php
+define('SO_LUONG_MOI_TRANG', 7);
 include_once("DataProvider.php");
 $sql = "SELECT MaHoa, TenHoa, GiaBan, Hinh FROM hoa";
+
+$trang = !isset($_REQUEST["p"]) ? 1 : $_REQUEST["p"];
+
+$sql .= " LIMIT ".(($trang - 1) * SO_LUONG_MOI_TRANG).", ".SO_LUONG_MOI_TRANG;
 $dsHoa = DataProvider::ExecuteQuery($sql);
-//print_r($dsHoa);
-while($hoa = $dsHoa->fetch())
+$stt = 1;
+while($row = $dsHoa->fetch())
 {
-	echo $hoa["TenHoa"]."<br>";
+	//echo "<br>".$stt.". ".$hoa["TenHoa"];
+	//$stt++;
+    $gia = number_format($row['GiaBan']);
+	$chuoi = <<< EOD
+<!--Một hàng hóa-->
+ <div class="hh-box">
+     <div class="hh-box-promotion"></div>
+     <div class="hh-box-qua"></div>
+     <img src="../hoa/{$row['Hinh']}" class="hh-box-image">
+     <img src="images/icon-new.png" class="hh-box-new" >
+     <div class="hh-box-name">{$row['TenHoa']}</div>
+     <div class="hh-box-gia">{$gia} đ</div>
+ </div>
+ <!--End Một hàng hóa-->
+EOD;
+echo $chuoi;
 }
 ?>
+<link href="Hoa.css" rel="stylesheet" />
