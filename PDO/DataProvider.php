0a824aa243f7e57482da24e3ed7bc8d1c055f4f8
Hien Luong <hyhien@gmail.com>
2020-06-08 08:16:32 +0700

    Fetch flowers

--- PDO/DataProvider.php
+++ PDO/DataProvider.php
@@ -1,15 +1,15 @@
-<?php
+﻿<?php
 class DataProvider 
 {
 	public static function ExecuteQuery($sql)
 	{
 		try{
-			$dsn = "mysql:host=localhost;dbname=qlbanhoat2";
+			$dsn = "mysql:host=localhost;dbname=qlbanhoa";
 			$dbh = new PDO($dsn, "root", "");
 			$dbh->query("SET NAMES utf8");
 			$result = $dbh->query($sql);
 		}catch(Exception $ex){
-		
+			echo "Lỗi: ";
 		}
 		finally{
 			$dbh = null;
---
+++ PDO/Home.php
@@ -0,0 +1,23 @@
+﻿<?php
+include_once('DataProvider.php');
+$sqlHoa = "SELECT MaHoa, TenHoa, GiaBan, Hinh FROM hoa";
+$dsHoa = DataProvider::ExecuteQuery($sqlHoa);
+while($row = $dsHoa->fetch())
+{
+    $gia = number_format($row['GiaBan']);
+    $chuoi = <<< EOD
+	<!--Một hàng hóa-->
+     <div class="hh-box">
+         <div class="hh-box-promotion"></div>
+         <div class="hh-box-qua"></div>
+         <img src="../hoa/{$row['Hinh']}" class="hh-box-image">
+         <img src="images/icon-new.png" class="hh-box-new" >
+         <div class="hh-box-name">{$row['TenHoa']}</div>
+         <div class="hh-box-gia">{$gia} đ</div>
+     </div>
+     <!--End Một hàng hóa-->
+EOD;
+	echo $chuoi;
+}
+?>
+<link href="Hoa.css" rel="stylesheet" />
