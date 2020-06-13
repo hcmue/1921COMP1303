1f05c46c53eb6cb14442dde91de6a3af34337933
Hien Luong <hyhien@gmail.com>
2020-06-13 08:33:39 +0700

    Add promotion condition

--- PDO/Lab08.php
+++ PDO/Lab08.php
@@ -1,7 +1,7 @@
 ﻿<?php
 define('SO_LUONG_MOI_TRANG', 7);
 include_once("DataProvider.php");
-$sql = "SELECT MaHoa, TenHoa, GiaBan, Hinh FROM hoa";
+$sql = "SELECT MaHoa, TenHoa, GiaBan, Hinh, GiamGia FROM hoa";
 if(isset($_REQUEST["ma_loai_hoa"]))
 {
     $sql .= " WHERE MaLoai = ".$_REQUEST["ma_loai_hoa"];
@@ -13,11 +13,17 @@ $dsHoa = DataProvider::ExecuteQuery($sql);
 $stt = 1;
 while($row = $dsHoa->fetch())
 {
-    $gia = number_format($row['GiaBan']);
+    $giaban = $row['GiaBan'] * ( 100 - $row["GiamGia"]) / 100;
+    $gia = number_format($giaban);    
+    $giamgia = '';
+    if($row["GiamGia"] > 0)
+    {
+        $giamgia = '<div class="hh-box-promotion"></div>';
+	}
 	$chuoi = <<< EOD
 <!--Một hàng hóa-->
  <div class="hh-box">
-     <div class="hh-box-promotion"></div>
+     {$giamgia}
      <div class="hh-box-qua"></div>
      <img src="../hoa/{$row['Hinh']}" class="hh-box-image">
      <img src="images/icon-new.png" class="hh-box-new" >
