24d381964d6d9805715b937b5a8f794b34c2b080
Hien Luong <hyhien@gmail.com>
2020-06-18 09:18:44 +0700

    Done

---
+++ PDO/GioHang.php
@@ -0,0 +1,38 @@
+﻿<?php
+session_start();
+if(!isset($_SESSION['MyCart']))
+{
+	die("Giỏ hàng rỗng");
+}
+?>
+<table>
+	<tr>
+		<td>Hình</td>
+		<td>Hoa</td>
+		<td>Giá</td>
+		<td>Số lượng</td>
+		<td>Thành tiền</td>
+		<td></td>
+	</tr>
+	<?php
+	include_once("DataProvider.php");
+	foreach($_SESSION['MyCart'] as $MaHoa => $SoLuong){
+		$rs= DataProvider::ExecuteQuery("SELECT MaHoa, TenHoa, Hinh, GiaBan FROM hoa WHERE MaHoa = $MaHoa");
+		$row = $rs->fetch();
+		$sum = $SoLuong * $row['GiaBan'];
+		$chuoi = <<< EOD
+	<tr>
+		<td><img src="../hoa/{$row['Hinh']}" height="80" /></td>
+		<td>{$row['TenHoa']}</td>
+		<td>{$row['GiaBan']}</td>
+		<td>
+			<input type="number" value="{$SoLuong}" />
+		</td>
+		<td>{$sum}</td>
+		<td></td>
+	</tr>
+EOD;
+		echo $chuoi;
+	}
+	?>
+</table>
---
+++ PDO/XLMuaHang.php
@@ -0,0 +1,31 @@
+﻿<?php
+session_start();
+if(!isset($_SESSION['MyCart']))
+{
+	header("Location: LocHoa.php");
+}
+
+try{
+	$dsn = "mysql:host=localhost;dbname=qlbanhoat5";
+	$dbh = new PDO($dsn, "root", "");
+	$dbh->query("SET NAMES UTF8");
+
+	$dbh->beginTransaction();
+
+	$sqlHD = "INSERT INTO `hoadon` (`MaHD`, `NgayDat`, `NoiGiao`, `MaKH`, `TinhTrang`) VALUES (NULL, now(), '280 ADV', '5', 'Mới đặt');";
+	$dbh->query($sqlHD);
+	$maHd = $db->lastInsertId();
+
+	foreach($_SESSION['MyCart'] as $MaHoa => $SoLuong){
+		$sqlCTHD = "INSERT INTO `chitiethd` (`SoDH`, `MaHoa`, `MaHD`, `SoLuong`) VALUES (NULL, '{$MaHoa}', '{$maHd}', '{$SoLuong}');";
+		$dbh->query($sqlCTHD);
+	}
+
+	$dbh->commit();
+	unset($_SESSION['MyCart']);
+}catch(Exception $ex){
+	$dbh->rollBack();
+}
+finally{
+	$dbh = null;
+}
